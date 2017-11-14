949ddfde2ae557e2fa9318b24a33268e07ff0bcb
Jinhyuk Lee <jin@playauto.co.kr>
2017-11-14 14:18:59 +0900

    Add autolinker, momentjs

--- src/build.js
+++ src/build.js
@@ -14,6 +14,7 @@ class Builder {
   
   render (templateName, locals = {}) {
     locals._ = _
+    locals.moment = require('moment')
     // TODO: check path if git-repo, npm package or not
     // add custom level handling
     const files = []
